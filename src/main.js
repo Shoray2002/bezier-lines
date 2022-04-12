3ceb8b9872c5a967c00b69a583e4c0dd6a6a1762
shoray singhal <shoryasinghall@gmail.com>
2022-04-12 16:41:15 +0530

    bug fix

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
-import * as THREE from "https://cdn.skypack.dev/three";
-import { OrbitControls } from "https://cdn.skypack.dev/three/examples/jsm/controls/OrbitControls.js";
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 import { bezier3 } from "../bezier3.mjs";
 // import of mjs module
 
